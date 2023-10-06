5b2d49678a8062dbad63f152fb873d2c1baf9470
Stephi <139639918+Stephi-x3@users.noreply.github.com>
2023-10-06 20:30:03 +0200

    particles

--- gameplay.js
+++ gameplay.js
@@ -31,6 +31,41 @@ function playRound(playerSelection, computerSelection, possibleShapes) {
     }
 }
 
+function fireworks(){
+    //https://confetti.js.org/more.html
+    const duration = 15 * 1000,
+    animationEnd = Date.now() + duration,
+    defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+    function randomInRange(min, max) {
+        return Math.random() * (max - min) + min;
+    }
+
+    const interval = setInterval(function() {
+        const timeLeft = animationEnd - Date.now();
+
+        if (timeLeft <= 0) {
+        return clearInterval(interval);
+        }
+
+    const particleCount = 50 * (timeLeft / duration);
+
+    // since particles fall down, start a bit higher than random
+    confetti(
+        Object.assign({}, defaults, {
+            particleCount,
+            origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+        })
+    );
+    confetti(
+        Object.assign({}, defaults, {
+            particleCount,
+            origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+        })
+    );
+    }, 250);
+}
+
 async function getPlayerChoice(cards, choice){
     //controller instead of removeEventListener
     const controller = new AbortController(); 
@@ -92,6 +127,7 @@ async function startGame(clickedButton){
         if(score_Player>score_Computer){
             document.getElementById('backgroundOfGame').style.backgroundImage = 'url(Pictures/congrats.png)';
             document.getElementsByClassName('congrats')[0].textContent = `Congratulations!`;
+            fireworks();
         }
     }, 1000);
 }
